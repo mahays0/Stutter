fab80803d45799a9dd8e948b055cf046b05cb40b
akelisakieva <andriana.kelisakieva@volarvideo.com>
2013-11-26 19:26:46 -0500

    Create BufferedReader.java

---
+++ BufferedReader.java
@@ -0,0 +1,20 @@
+
+public class BufferedReader {
+
+
+	public BufferedReader(FileReader myFile) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BufferedReader(InputStreamReader inputStreamReader) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String readLine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
